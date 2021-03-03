b81f3d94c69b2964cbb74259ed066f076f6a4f4a
HananMohammed <hananmohammed2468@gmail.com>
2021-03-03 14:41:50 +0200

    Add Mocha Assert Function Test

---
+++ prod/users/test/create_test.js
@@ -0,0 +1,13 @@
+// **************Mocha***********************
+// write Some test to make Sure Can Create
+// New User and save it to database
+// ******************************************
+//it's A function to make an Assertion
+const assert = require('assert');
+
+describe('Creating Records', () => {
+    it('Saves a user', () => {
+        //Create Actual Assertion
+        assert(1+1 === 3);
+    });
+});
