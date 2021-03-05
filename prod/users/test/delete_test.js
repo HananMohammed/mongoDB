ac039e16344f33e0faaa4662428f1fb809ece712
HananMohammed <hananmohammed2468@gmail.com>
2021-03-05 18:10:09 +0200

    Class Based Removes Record

---
+++ prod/users/test/delete_test.js
@@ -0,0 +1,45 @@
+const assert = require('assert');
+const User = require('../src/user')
+
+describe('Deleting a User', () => {
+
+    let joe;
+
+    beforeEach((done) => {
+
+        joe = new User({
+            name: 'Joe'
+        });
+
+        joe.save()
+            .then(() => done() )
+    });
+
+    // it('Model instance remove', (done) => {
+    //     joe.remove()
+    //     .then(() => User.findOne({name: 'Joe'}) )
+    //     .then((user) => {
+    //         assert(user === null);
+    //         done();
+    //     })
+    // });
+
+    it('Class Method remove', (done) => {
+        //Remove a bunch of records with some given criteria
+        //at the same time
+        User.remove({name:'Joe'})
+            .then(() => User.findOne({name: 'Joe'}) )
+            .then((user) => {
+                assert(user === null);
+                done();
+            })
+    });
+
+    it('Class method findAndRemove ', () => {
+
+    });
+
+    it('Class method findByIdAndRemove ', () => {
+
+    });
+})
