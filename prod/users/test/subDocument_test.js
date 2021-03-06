acbd7c2fce66c482f1cc909612e036461d86c463
HananMohammed <hananmohammed2468@gmail.com>
2021-03-06 13:43:41 +0200

    Adding Subdocuments to Existing Records

--- prod/users/test/subDocument_test.js
+++ prod/users/test/subDocument_test.js
@@ -15,4 +15,23 @@ describe('subDocument Test ', () => {
                 done();
             })
     });
-})
+
+    it('Can Add  Sub Document To an existing Record', (done) => {
+        const joe = new User({
+            name: 'Joe',
+            posts: []
+        })
+        joe.save()
+            .then(() => User.findOne({name: 'Joe'}))
+            .then((user) => {
+                user.posts.push({title: 'New Post' });
+                return user.save();
+            })
+            .then(() => User.findOne({name: 'Joe'}))
+            .then((user) => {
+                assert(user.post[0].title === 'New Post');
+            })
+        done();
+
+    });
+    })
